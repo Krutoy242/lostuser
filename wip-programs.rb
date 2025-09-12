96e3a87e97b2578d70cf9d072d451afff8ef5796
Krutoy242 <Krutoy242@gmail.com>
2025-09-12 11:15:16 +0300

    chore: update WIP programs

--- wip-programs.rb
+++ wip-programs.rb
@@ -24,3 +24,28 @@ IgSI(3,k).mS^_{IsF/3/k|'Pp1,sel9,Rp1,Rd^sel1'/R~-Rd/1}
 _"IgSI(3,_a^i117).mS^_{IsF/3/a|'Pp1,sel9,Rp1,Rd^sel1'/R~-Rd/1}"
 
 IgSI(3,_a^i117).mS^_{IsF/3/a|'Pp1,sel9,Rp1,Rd^sel1'/R~-Rd/1}
+
+# ?========================================================
+# Cyclic miner but with blocks
+
+# With infinity placing block
+Gi,_'Rm3,Rp3,Rsw3,Rp0'~i*2.25,Rtn⒯
+
+# Twilight Forest Bedrock
+Gi,_'~(Rsw/0),Rsw3,Rm3'~i*0.5,Rtn⒯
+
+# ?========================================================
+# Hammer Leveller
+# Cobble in front, Mechanical Crafter with Sharpening Kits under
+Rsw3,i%99==0ⓐ{Ie!,_a(I23^1).mD-a.d<20ⓐ{Rd0,s2,Rsk0},Ie!}
+
+# ?========================================================
+# Flower gatherer
+# 0 slot: petal, 1 slot: empty, 2 slot: Kama, 3+ slots: fertilizer
+Ru3,Ie!,_a⒯,_'aⓐRc&k+2>0ⓐ{R16&k+2,Ie!,Ru3,Ie!,_a⒡,Rsel1,Ie!}'~14
+
+Ru3,Ie!,Ru3,R16&2,Ie!,Rsw3
+
+R16/(i%3+1)-Ie-i/_{Ru,Ru,Rsw}/3-Ie
+R16(i%3+1),Ie!,i/_{Ru,Ru,Rsw}&3,Ie!
+_m(i%3+1),R16^m,Ie!,({Ru,Ru,Rsw})[m]&3,Ie!
